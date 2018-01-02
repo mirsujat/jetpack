c4627e074dee4e3693aeee10103e4609126beb24
MirSujat <MirSujat>
2018-01-02 16:46:00 +0600

    complete jetpack project

---
+++ src/components/Items.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Item from './Item';
+import Filter from './Filter';
+
+class Items extends Component {
+  state = {
+    searchTerm: ''
+  };
+
+  updateSearchTerm = searchTerm => {
+    this.setState({ searchTerm });
+  };
+
+  render() {
+    const { title, items, onRemove, onToggle } = this.props;
+    return (
+      <section className="Items">
+        <h2>
+          {title} ({items.length})
+        </h2>
+        <Filter searchTerm={this.state.searchTerm} onChange={this.updateSearchTerm} />
+        {items
+          .filter(item =>
+            // Hmm… this needs some work.
+            item.value.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+          )
+          .map(item => (
+            <Item key={item.id} onToggle={onToggle} onRemove={() => onRemove(item)} item={item} />
+          ))}
+      </section>
+    );
+  }
+}
+
+export default Items;
